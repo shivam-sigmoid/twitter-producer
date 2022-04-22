197befb2aec10f29bd56ef91907b126b44a80345
Shivam Raj <shivamraj@AS-MAC-0208.local>
2022-04-22 15:18:53 +0530

    added source for api 5

---
+++ app/testing_api_for_5.py
@@ -0,0 +1,12 @@
+import json
+
+import requests as r
+
+response = r.get("http://covidsurvey.mit.edu:5000/query?country=all&signal=measures_taken")
+
+json_data = json.loads(response.text)
+
+# print(json_data['AE']['measures_taken'])
+
+for k in json_data['AE']['measures_taken']:
+    print(k)

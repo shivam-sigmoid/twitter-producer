4c0fb94669a7f69046c28ebf7f7d575e33d31a44
Shivam Raj <shivamraj@AS-MAC-0208.local>
2022-04-22 12:02:52 +0530

    get country done

--- app/testing_api_data.py
+++ app/testing_api_data.py
@@ -8,10 +8,12 @@ def get_country(loc):
         user_ag = 'user_me_{}'.format(randint(10000, 99999))
         geolocator = Nominatim(user_agent=user_ag)
         location = geolocator.geocode(loc)
-        temp = location.address
-        temp = temp.split(',')
-        return temp[-1].lstrip()
+        address = location.address
+        address_split = address.split(',')
+        country = address_split[-1].lstrip()
+        return country
     except:
         return loc
 
 
+print(get_country("khagaul"))

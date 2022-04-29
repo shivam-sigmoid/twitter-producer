386572090b1f150ca35e11883c6a8e7a58f621b5
Shivam Raj <shivamraj@AS-MAC-0208.local>
2022-04-29 11:15:08 +0530

    Done

--- app/data_api_6.py
+++ app/data_api_6.py
@@ -8,28 +8,5 @@
 # for tweet in tweets:
 #     print(tweet)
 
-def throw_if_mongodb_is_unavailable(host, port):
-    import socket
-    sock = None
-    try:
-        sock = socket.create_connection(
-            (host, port),
-            timeout=1) # one second
-    except socket.error as err:
-        raise EnvironmentError(
-            "Can't connect to MongoDB at {host}:{port} because: {err}"
-            .format(**locals()))
-    finally:
-        if sock is not None:
-            sock.close()
-
-# elsewhere...
-HOST = 'localhost'
-PORT = 27017
-throw_if_mongodb_is_unavailable(HOST, PORT)
-import pymongo
-conn = pymongo.MongoClient(HOST, PORT)
-print(conn.admin.command('ismaster')['ok']==1.0)
-# etc.
 
  # db.tweets.find({ full_text: { $regex: /(donation|contribution|contri)/i } })
